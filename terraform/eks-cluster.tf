30c8c77a5fc1a60c590e69f8eda63e92239069a1
prasadnaik1-admin <prasadsnaik18@gmail.com>
2025-08-23 15:48:23 +0530

    added

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,21 +5,24 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
+
+  # Disable KMS and cluster encryption
+  enable_irsa = true
+  manage_aws_auth = true
+  cluster_kms_key_id = null   # No KMS key used
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 3
       desired_size = 2
@@ -27,13 +30,13 @@ module "eks" {
 
     two = {
       name = "node-group-2"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 2
       desired_size = 1
     }
   }
+
+  # Optional: disable cluster log types to avoid KMS
+  cluster_enabled_log_types = [] 
 }
-########
