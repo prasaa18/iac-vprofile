a282c16a3d3b08c3507c417dd9cf95ac359d9f71
prasadnaik1-admin <prasadsnaik18@gmail.com>
2025-08-23 15:57:22 +0530

    added

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,9 +5,13 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
+
+  # Remove KMS references
+  cluster_encryption_config = []
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
